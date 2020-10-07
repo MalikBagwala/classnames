b14d2e2d4f949753a9eaa080a0a36c6b5ef3ab93
MalikBagwala <malikeditz@gmail.com>
2020-10-07 12:02:37 +0530

    added tests and core functionality

---
+++ index.test.js
@@ -0,0 +1,13 @@
+const classNames = require(".");
+
+it("Core Tests", () => {
+  expect(classNames("abc", "123")).toEqual("abc 123");
+  expect(classNames("abc 123")).toEqual("abc 123");
+  expect(classNames("abc", { 123: true })).toEqual("abc 123");
+  expect(classNames("")).toEqual(undefined);
+  expect(classNames()).toEqual(undefined);
+  expect(classNames(null, "123", "abc")).toEqual("123 abc");
+  expect(classNames(0, false, "abc", undefined, "123", NaN)).toEqual("abc 123");
+  expect(classNames("abc", "123", { abc: true })).toEqual("abc 123");
+  expect(classNames("abc", "123", ["xyz", "pqr"])).toEqual("abc 123");
+});
