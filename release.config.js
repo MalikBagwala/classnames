6cddf0c4574de68f81f86b1ec6993d4d43658f95
Malik Bagwala <m.bagwala@gmail.com>
2020-12-07 12:34:37 +0530

    feat(packages): updated libraries and setup release cycle

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  release: {
+    plugins: [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+      "@semantic-release/github",
+      [
+        "@semantic-release/git",
+        {
+          assets: ["CHANGELOG.md", "package.json"],
+          message:
+            "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        },
+      ],
+    ],
+    branches: ["main"],
+  },
+};
